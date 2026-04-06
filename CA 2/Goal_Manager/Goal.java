3496de6fdd4a5c4bf23bfbd430a100774c4a0434
shubham45t <s.hubham93972540@gmail.com>
2026-04-06 15:06:17 +0530

    Added package structure and improved Goal class design

--- CA 2/Goal_Manager/Goal.java
+++ CA 2/Goal_Manager/Goal.java
@@ -1,25 +1,39 @@
+package goalmanager;
+
 public class Goal {
 
     private int id;
+
     private String title;
+
     private String description;
 
-    public Goal(int id, String title, String description){
+    public Goal(int id,String title,String description){
+
         this.id=id;
+
         this.title=title;
+
         this.description=description;
+
     }
 
     public int getId(){
+
         return id;
+
     }
 
     public String getTitle(){
+
         return title;
+
     }
 
     public String getDescription(){
+
         return description;
+
     }
 
 }
